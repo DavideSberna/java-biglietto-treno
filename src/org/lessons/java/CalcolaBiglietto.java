4cf2dcb80b01e94361890553f3498623222122ca
DavideSberna <sbernadavide@gmail.com>
2023-09-13 17:58:12 +0200

    add new control to input

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class CalcolaBiglietto {
 	
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
 		double priceKm = 0.21;
-		double discount = 0.0; 
+		double discount = 1; 
 		double totalPrice = 0.0;
 		int km = 0;
 		int age = 0;
@@ -15,20 +15,33 @@ public class CalcolaBiglietto {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Quanti chilometri vuoi percorrere? ");
+		
+		while (!sc.hasNextInt()) {
+            System.out.print("Inserisci un valore numerico. Quanti chilometri vuoi percorrere? ");
+            sc.next();
+        }
+		
 		km = sc.nextInt();
 		
 		System.out.print("Quanti anni hai? ");
+		
+		 while (!sc.hasNextInt()) {
+            System.out.print("Inserisci un valore numerico. Quanti anni hai? ");
+            sc.next();
+	     }
+		
 		age = sc.nextInt();
+		
 		
 		totalPrice = km * priceKm;
 		
 		if (age < 18) {
-			discount = 0.2; 
+			discount = 0.8; 
 		} else if (age >= 65) {
-			discount = 0.4; 
+			discount = 0.6; 
 		}
 		
-		double finalPrice = totalPrice * (1 - discount);
+		double finalPrice = totalPrice * discount;
 		
 		System.out.println("Il prezzo del biglietto è: " + finalPrice + " €");
 	}
